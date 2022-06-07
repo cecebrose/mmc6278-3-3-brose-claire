cc4c13a9b4eaf4ca056ed31e028ef44a63f959a7
cecebrose <clairebrose@ufl.edu>
2022-06-06 20:50:37 -0400

    made questions t/f

--- index.js
+++ index.js
@@ -1,169 +1,35 @@
-var questionsArr = [
-    {
-        numb: 1,
-    question: "What was the first toy to be advertised on TV?",
-    answer: "Mr. Potato Head",
-    options: [
-      "Barbie",
-      "Mr. Potato Head",
-      "G.I. Joe",
-      "Teddy Ruxpin"
-    ]
-  },
+function runQuiz() {
+    var questionsArr = [
+        {
+            question: "Peanuts aren't nuts" ,
+            answer: "True",
 
-    {
-        numb: 2,
-    question: "How many pedals do most modern pianos have?",
-    answer: "Three",
-    options: [
-      "One",
-      "Two",
-      "Three",
-      "None"
-    ]
-  },
-    {
-        numb: 3,
-        question: "What was the first feature-length animated movie ever released?",
-        answer: "Snow White and The Seven Dwarfs",
-        options: [
-          "The Little Mermaid",
-          "Cinderella",
-          "Snow White and The Seven Dwarfs",
-          "Toy Story"
-        ]
-      },
+            question: "Emus can't fly",
+            answer: "True",
 
-      {
-          numb: 4,
-          question: "Where is Area 51 located?",
-          answer: "Nevada",
-          options: [
-              "New Mexico",
-              "Nevada",
-              "Arizona",
-              "Texas"
-          ]
-      },
+            question: "The Atlantic Ocean is the largest ocean",
+            answer: "False",
 
-      {
-          numb: 5,
-          question: "What is the loudest animal on earth?",
-          answer: "Sperm Whale",
-          options: [
-              "Elephant",
-              "Sperm Whale",
-              "Tiger",
-              "Humans"
-          ]
-      }
-    ];
+            question: "Penguins aren't found in Africa",
+            answer: "False",
 
-function runQuiz(){
-    // if runQuiz button clicked
-    runQuiz.onclick = ()=>{
-        info_box.classList.add("activeInfo"); //show info box
-    }
-}
+            question: "Mount Kilimanjaro is the highest mountain in the world",
+            answer: "False",
+        }
+      ];
 
-let timeValue =  15;
-let que_count = 0;
-let que_numb = 1;
-let userScore = 0;
-let counter;
-let counterLine;
-let widthValue = 0;
-
-// if Next Que button clicked
-next_btn.onclick = ()=>{
-    if(que_count < questions.length - 1){ //if question count is less than total question length
-        que_count++; //increment the que_count value
-        que_numb++; //increment the que_numb value
-        showQuetions(que_count); //calling showQestions function
-        queCounter(que_numb); //passing que_numb value to queCounter
-        clearInterval(counter); //clear counter
-        clearInterval(counterLine); //clear counterLine
-        startTimer(timeValue); //calling startTimer function
-        startTimerLine(widthValue); //calling startTimerLine function
-        timeText.textContent = "Time Left"; //change the timeText to Time Left
-        next_btn.classList.remove("show"); //hide the next button
-    }else{
-        clearInterval(counter); //clear counter
-        clearInterval(counterLine); //clear counterLine
-        showResult(); //calling showResult function
-    }
-};
-
-// getting questions and options from array
-function questionsArr(){
-    //creating a new span and div tag for question and option and passing the value using array index
-    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
-    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
-    + '<div class="option"><span>'+ questions[index].options[4] +'</span></div>';
-    + '<div class="option"><span>'+ questions[index].options[5] +'</span></div>';
-    que_text.innerHTML = que_tag; //adding new span tag inside que_tag
-    option_list.innerHTML = option_tag; //adding new div tag inside option_tag
     
-    const option = option_list.querySelectorAll(".option");
-    // set onclick attribute to all available options
-    for(i=0; i < option.length; i++){
-        option[i].setAttribute("onclick", "optionSelected(this)");
-    }
-}
 
-//if user clicked on option
-function optionSelected(answer){
-    clearInterval(counter); //clear counter
-    clearInterval(counterLine); //clear counterLine
-    let userAns = answer.textContent; //getting user selected option
-    let correcAns = questions[que_count].answer; //getting correct answer from array
-    const allOptions = option_list.children.length; //getting all option items
-    
-    if(userAns == correctAns){ //if user selected option is equal to array's correct answer
-        userScore += 1; //upgrading score value with 1
-        answer.classList.add("correct"); //adding green color to correct selected option
-        answer.insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to correct selected option
-        console.log("Correct Answer");
-        console.log("Your correct answers = " + userScore);
-    }else{
-        answer.classList.add("incorrect"); //adding red color to correct selected option
-        answer.insertAdjacentHTML("beforeend", crossIconTag); //adding cross icon to correct selected option
-        console.log("Wrong Answer");
-        for(i=0; i < allOptions; i++){
-            if(option_list.children[i].textContent == correcAns){ //if there is an option which is matched to an array answer 
-                option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
-                option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
-                console.log("Auto selected correct answer.");
-            }
+    var numOfAgrees = 2;
+    for(var i = 2; i < questions.length; i++) {
+        var question = questions [i]
+        var answer = confirm(question)
+        if (answer) {
+            numOfAgrees++
         }
     }
-    for(i=0; i < allOptions; i++){
-        option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
-    }
-    next_btn.classList.add("show"); //show the next button if user selected any option
-};
-
-function showResult(){
-    info_box.classList.remove("activeInfo"); //hide info box
-    quiz_box.classList.remove("activeQuiz"); //hide quiz box
-    result_box.classList.add("activeResult"); //show result box
-    const scoreText = result_box.querySelector(".score_text");
-    if (userScore > 100){ // if user scored more than 50
-        //creating a new span tag and passing the user score number and total question number
-        let scoreTag = '<span>and congrats! , You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
-        scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text
-    }
-    else if(userScore > 50){ // if user scored more than 0
-        let scoreTag = '<span>and nice , You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
-        scoreText.innerHTML = scoreTag;
-    }
-    else{ // if user scored less than 50
-        let scoreTag = '<span>and sorry , You got only <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
-        scoreText.innerHTML = scoreTag;
-    }
-};
-
-// resource: https://www.codingnepalweb.com/quiz-app-with-timer-javascript //
+      if (numOfAgrees >=2){
+      alert("Smarty pants!")
+    } else {
+      alert("Better luck next time!")
+    };
